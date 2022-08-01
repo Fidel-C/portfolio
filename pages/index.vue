3c9205aa0f2c6550aec5c274ee6bdc7663d2db13
fidelc <fidelchukwunyere@gmail.com>
2022-08-01 22:16:30 +0100

    ui changes

--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,20 @@
   <section class="section" >
    <div class="p-4">
 
-     <div class="container has-background-black p-2" style="border-radius:30%;">
+     <div class="has-background-black p-2  is-justify-content-center" style="border-radius:30%;">
 
        <h1 class="title is-2 has-text-warning has-text-centered is-italic">All<span class="has-text-light is-underlined">things beautiful</span> </h1>
-       <b-icon icon="handshake" class="ml-3 has-text-warning" size="is-large"></b-icon>
+<div class="has-text-centered">
+       <b-icon icon="handshake" class="has-text-warning has-text-centered" size="is-large"></b-icon>
 
-<div class="section">
+</div>
+<div class="section has-text-centered">
         <b-carousel :arrow="arrow" >
           <b-carousel-item  v-for="(item, i) in items" :key="i">
             <section :class="`hero is-${item}`">
 
-           <div class="hero-body" style="max-height:200px;max-width:300px;">
-             <div class="">
+           <div class="hero-body has-text-centered" style="max-height:200px;max-width:300px;">
+             <div class="has-text-centered">
                    <b-image
                :src="item.imgurl"
                :responsive="responsive"
