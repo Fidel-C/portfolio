acd9e9113880e30e97fa0c00015805c839d8620c
fidelc <fidelchukwunyere@gmail.com>
2022-08-27 01:10:00 +0100

    fixed card display

--- pages/services/index.vue
+++ pages/services/index.vue
@@ -1,15 +1,15 @@
 <template>
   <section class="container">
-    <p class="is-subtitle has-text-centered is-italic p-4 has-text-light">
+    <p class="is-subtitle is-bold has-text-centered is-italic p-4 has-text-light">
      {{service.details}}</p>
 
       <div v-if="service.title=='Projects Showcase'"  class="mx-auto">
 
       <div class="menu-list is-hoverable">
         <ol>
-          <div class="list-item">
+          <div class="list-item my-2">
           <li v-for="project in service.projects" :key="project.url">
-           <a :href="project.url" target="_blank" class="has-text-centered has-text-warning">{{project.desc}}</a>
+           <a :href="project.url" target="_blank" class="is-italic has-text-warning">{{project.desc}}</a>
           </li>
        </div>
         </ol>
