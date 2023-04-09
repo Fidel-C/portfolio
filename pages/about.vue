f6c2ea92cfc247e10c7f0f0fa64321473dad1d81
fidelc <fidelchukwunyere@gmail.com>
2023-04-09 22:00:40 +0100

    removed footer

--- pages/about.vue
+++ pages/about.vue
@@ -3,7 +3,7 @@
 
   <div class="is-rounded mx-auto p-2 col-sm-10 col-md-8 col-lg-6 col-xl-6" style="border-radius:30%;  background-image: linear-gradient(rgb(47, 3, 47),black);box-shadow: darkslateblue;">
         <h2 class="title is-6 is-bold has-text-light has-text-centered mb-1 is-underlined"> About the Author</h2>
-    <div>
+    <div class="p-3">
       <p class="subtitle is-bold is-6 is-italic is-semibold has-text-light has-text-centered">
     I am Fidel, a self-taught software developer.<br>
     I have many hobbies one of which is coding. I'm experienced in both the frontend and
@@ -13,17 +13,18 @@
     <br> Please feel free to reach me on any one of the links on the <NuxtLink class="subtitle is-5 is-italic has-text-warning is-link" :to="{name:'contact'}">contact page</NuxtLink>.
     <br> Thanks a bunch.</p>
 
-   <div class="main-footer">
-      <div class="mx-auto" style="width:70px;height:40px;">
-        <b-image src="/logo.png" ratio="16by9" :responsive="responsive" is-rounded> </b-image>
-      </div>
 
-
-      <div class="has-text-centered"> &copy; 2020 - {{ new Date().getFullYear() }} </div>
-
-    </div>
   </div>
 </div>
+   <div class="main-footer">
+        <div class="mx-auto" style="width:70px;height:40px;">
+          <b-image src="/logo.png" ratio="16by9" :responsive="responsive" is-rounded> </b-image>
+        </div>
+
+
+        <div class="has-text-centered"> &copy; 2020 - {{ new Date().getFullYear() }} </div>
+
+      </div>
 </div>
 </template>
 
