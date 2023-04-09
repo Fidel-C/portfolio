1ed04a9f447b661233b60f33265123847790fbaf
fidelc <fidelchukwunyere@gmail.com>
2023-04-09 10:16:51 +0100

    added 2 50dvh divs to contact and index

--- layouts/default.vue
+++ layouts/default.vue
@@ -115,17 +115,11 @@ position: relative;
 }
 
 .layout {
-  position: absolute;
-  top:0;
-  left:0;
-  bottom: 0;
-  right: 0;
   overflow:scroll;
   height:100dvh;
   width:100dvw;
    background-image: radial-gradient(orange 10%,black 90%);
   color: wheat;
-  position: relative;
 
 
 }
